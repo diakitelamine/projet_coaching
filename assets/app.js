d1c95c6607c52e6170f5c263ec742cea7717e279
diakitelamine <diakitelamine555@gmail.com>
2023-01-09 16:55:10 +0100

    config react

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,16 @@
-import React from 'React';
 import  ReactDOM  from 'react-dom';
-
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import React from 'react';
 import './styles/app.css';
-
-// start the Stimulus application
 import './bootstrap';
+import NavBar from './components/NavBar';
+
 
 console.log("Hello word!!!")
 
 const App = () =>{
-    return <h1> Bonjour a tous </h1>;
+    return <> 
+          <NavBar/>
+     </>;
 }
 
 // Dans la div app rend moi le contenu de App
