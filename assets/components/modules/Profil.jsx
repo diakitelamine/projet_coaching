9801e05bc4ad0609b1a3cce709e4428a1de47b10
Wasef <alex.wasef@gmail.com>
2023-05-08 18:41:31 +0200

    user can have manu images

--- assets/components/modules/Profil.jsx
+++ assets/components/modules/Profil.jsx
@@ -1,16 +1,44 @@
 import React, { useEffect } from 'react';
 import Auth from './Auth';
+import { API_URL } from '../../config';
 const Profil = () => {
+    const [id, setId] = React.useState("");
 
     useEffect(() => {
-        console.log(sessionStorage);
+        // Requete à l'api user
+        fetch(API_URL+'user/'+sessionStorage.getItem("id"))
+        // Transforme les données en json
+        .then((res) => res.json())
+        .then((json) => {
+            setId(json.id);
+            let path = getImageByUser(id);
+            path.then((value) => {
+                console.log(value);
+            })
+        });
     }, [])
 
-        
+    async function getImageByUser(id) {
+        const path = await fetch(API_URL+'image/user/'+sessionStorage.getItem("id"))
+        // Transforme les données en json
+        .then((res) => res.json())
+        .then((json) => {
+            if (json != null) {
+               return json.path;   
+            }
+            else{
+                return 'default.svg'; 
+            } 
+        });
+        return path;
+    }
+
     return(
         <div>
             <Auth></Auth>
-            <p>Page profil</p>
+            <div className="cover">
+                <p>Page profil</p>
+            </div>
         </div>
     )
 }
